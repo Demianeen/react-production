4eba5124cede84c52053055b62c24cd9217e7c7c
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-03-01 14:38:21 +0000

    Add Navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,12 +3,14 @@ import "./styles/index.scss";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 import { AppRouter } from "app/providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
+      <Navbar />
       <AppRouter />
     </div>
   );
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import styles from "./Navbar.module.scss";
+
+interface NavbarProps {
+  className?: string;
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+  return (
+    <div className={classNames(styles.navbar, {}, [className])}>
+      <div className={styles.links}>
+        <AppLink
+          theme={AppLinkTheme.INVERTED}
+          to={"/"}
+          className={styles.homeAppLink}
+        >
+          Home
+        </AppLink>
+        <AppLink theme={AppLinkTheme.INVERTED} to={"/about"}>
+          About
+        </AppLink>
+      </div>
+    </div>
+  );
+};
