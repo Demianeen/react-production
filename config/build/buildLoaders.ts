32990b39d591ed72ca8cb1a19365d3b38ef4f91a
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-04-14 14:17:42 +0100

    Fix bug with not working React refresh plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,31 +2,14 @@ import type webpack from 'webpack'
 import type { BuildOptions } from './types/config'
 import { buildCssLoader } from './loaders/buildCssLoader'
 import { buildSvgLoader } from './loaders/buildSvgLoader'
+import { buildBabelLoader } from './loaders/buildBabelLoader'
 
 export default ({
   isDev,
 }: BuildOptions): webpack.RuleSetRule[] => {
   const svgLoader = buildSvgLoader()
 
-  const babelLoader = {
-    test: /\.(js|jsx|ts|tsx)$/,
-    exclude: /node_modules/,
-    use: {
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env'],
-        plugins: [
-          [
-            'i18next-extract',
-            {
-              locales: ['en', 'ua'],
-              keyAsDefaultValue: true,
-            },
-          ],
-        ],
-      },
-    },
-  }
+  const babelLoader = buildBabelLoader(isDev)
 
   const cssLoader = buildCssLoader(isDev)
 
