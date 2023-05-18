c6a3737213121337f35874901ec96054d94b545a
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-05-18 10:39:51 +0100

    Add html report for unit tests

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -61,6 +61,20 @@ export default {
     ),
   },
 
+  // Use this configuration option to add custom reporters to Jest
+  reporters: [
+    'default',
+    [
+      'jest-html-reporters',
+      {
+        publicPath: './html-report',
+        filename: 'report.html',
+        openReport: true,
+        inlineSource: true,
+      },
+    ],
+  ],
+
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -127,9 +141,6 @@ export default {
 
   // Run tests from one or more projects
   // projects: undefined,
-
-  // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
 
   // Automatically reset mock state before every test
   // resetMocks: false,
