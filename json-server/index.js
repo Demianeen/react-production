8c84cf9511b44083a55bd4ebb417af9b4c2c4597
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-06-23 09:49:04 +0300

    Add https for backend, remove apiUrl in build:prod

--- json-server/index.js
+++ json-server/index.js
@@ -1,12 +1,16 @@
 const fs = require('fs')
 const jsonServer = require('json-server')
 const path = require('path')
+const https = require('https')
 
 const server = jsonServer.create()
 
 const router = jsonServer.router(
   path.resolve(__dirname, 'db.json')
 )
+
+const HTTP_PORT = 8000
+const HTTPS_PORT = 8443
 
 server.use(jsonServer.defaults({}))
 server.use(jsonServer.bodyParser)
@@ -63,6 +67,19 @@ server.use((req, res, next) => {
 server.use(router)
 
 // start server
-server.listen(8000, () => {
-  console.log('server is running on 8000 port')
+server.listen(HTTP_PORT, () => {
+  console.log(`http server is running on ${HTTP_PORT} port`)
 })
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/mybrandview.co.uk/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/mybrandview.co.uk/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/mybrandview.co.uk/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+const httpsServer = https.createServer(credentials, server)
+httpsServer.listen(HTTPS_PORT, () => console.log(`https server listening on ${HTTPS_PORT} port`))
