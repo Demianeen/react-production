e1ac75690288e01f15d13cc8f9cda9ec042ec306
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-07-14 11:02:56 +0100

    Remove async loki callback

--- src/pages/ProfilePage/ui/ProfilePage/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage/ProfilePage.stories.tsx
@@ -2,17 +2,11 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { RoutePath } from '@/shared/const/router/routePath'
 import { routes } from '@/shared/lib/router/routes'
 import { LayoutDecorator } from '@/shared/lib/storybook/LayoutDecorator'
-import createAsyncCallback from '@loki/create-async-callback'
 import ProfilePage from './ProfilePage'
-
-export const AsyncStory = () => (
-  // @ts-expect-error test
-  <ProfilePage onDone={createAsyncCallback()} />
-)
 
 export default {
   title: 'pages/ProfilePage',
-  component: AsyncStory,
+  component: ProfilePage,
   argTypes: {
     backgroundColor: { control: 'color' },
   },
