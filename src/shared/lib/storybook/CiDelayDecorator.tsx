27bab2a87d066edc3f664b0acf56e61320067124
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-07-31 07:24:52 +0100

    Test ci delay decorator

--- src/shared/lib/storybook/CiDelayDecorator.tsx
+++ src/shared/lib/storybook/CiDelayDecorator.tsx
@@ -1,5 +1,4 @@
 import type { StoryFn } from '@storybook/react'
-import { LokiDelayDecorator } from './LokiDelayDecorator'
 
 /**
  * Makes a delay between capturing screenshot. Works only in CI environment. Otherwise just renders the story component.
@@ -7,7 +6,8 @@ import { LokiDelayDecorator } from './LokiDelayDecorator'
 export const CiDelayDecorator = (StoryComponent: StoryFn) => {
   // runs only on CI
   if (process.env.GITHUB_ACTIONS) {
-    return LokiDelayDecorator()(StoryComponent)
+    throw new Error('Not implemented')
+    // return LokiDelayDecorator()(StoryComponent)
   }
 
   return <StoryComponent />
